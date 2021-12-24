edf1628c968a7d1efacb2af7e27c8cf4aedbe5c8
CZQ815 <70275011+CZQ815@users.noreply.github.com>
2021-12-24 15:26:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ password = 'xxxxxxxxxx'
 pushplus_token = 'xxxx'
 
 if __name__ == '__main__':
+    
     daka = ClockIn.DAKA(username, password, pushplus_token)
-    i = 1
-    while i <= 6:
+    
+    for i in range(6):
         result = daka.run()
         if result == True:
             break
-        i = i + 1
+    
     if result == True:
         daka.pushplus('打卡成功！')
     else:
